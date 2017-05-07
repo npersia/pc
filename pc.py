2be14473ccda9af3e54a8b42d9cc6b840ca29991
Nahuel Persia <npersia@localhost.localdomain>
2017-05-07 02:42:53 -0300

    subo todo, agregado de la base de datos

--- pc.py
+++ pc.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import pymysql
+
 
 # geti es un ejemplo
 def geti():
@@ -107,10 +109,74 @@ def getProd(comercios):
 c = getComercios(nivel=3)
 print(c)
 idC = getIdComercios(c)
-print(idC)
+#print(idC)
 #print(getProductos(idC))
 #print(len(idC))
 #prods=getProd(idC)
 #print(prods)
 #print(len(prods))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ejemplo de conexion y uso de mysql http://www.w3big.com/es/python3/python3-mysql.html
+#docker run --rm --name cp_bd -p 3306:3306 -e MYSQL_ROOT_PASSWORD=npersia -e MYSQL_DATABASE=cp -e MYSQL_USER=cp -e MYSQL_PASSWORD=cp mysql
+#db=pymysql.connect("localhost","cp","cp","cp" )
+#cursor = db.cursor()
+#cursor.execute("SELECT VERSION()")
+#data = cursor.fetchone()
+#print ("Database version : %s " % data)
+#db.close()
+
+
+
+
+
+def setDB():
+    HOST = "localhost"
+    USER = "cp"
+    PASS = "cp"
+    DB = "cp"
+    return pymysql.connect("localhost","cp","cp","cp" )
+
+
+def setTabComercios(cursor):
+    cursor.execute("CREATE TABLE Comercios(
+    sucursalNombre varchar(255),
+    provincia varchar(255),
+    localidad varchar(255)
+    lng varchar(255),
+    lat varchar(255),
+    sucursalTipo varchar(255),
+    banderaDescripcion varchar(255),
+    comercioId int,
+    distanciaDescripcion varchar(255),
+    comercioRazonSocial varchar(255),
+    sucursalId varchar(255),
+    distanciaNumero double,
+    banderaId int,
+    id varchar(255),
+    direccion varchar(255)
+    );")
+
+
+def setComerciosInTab(cursor,comercios):
+    for c in comercios:
+"INSERT INTO Comercios(sucursalNombre,provincia,localidad,lng,lat,sucursalTipo,banderaDescripcion,comercioId,distanciaDescripcion,comercioRazonSocial,sucursalId,distanciaNumero,banderaId,id,direccion)
+VALUES("+str(c["sucursalNombre"])+","+str(c["provincia"])+","+str(c["localidad"])+","+str(c["lng"])+","+str(c["lat"])+","+str(c["sucursalTipo"])+","+str(c["banderaDescripcion"])+","+str(c["comercioId"])+","+str(c["distanciaDescripcion"])+","+str(c["comercioRazonSocial"])+","+str(c["sucursalId"])+","+str(c["distanciaNumero"])+","+str(c["banderaId"])+","+str(c["id"])+","+str(c["direccion"]))"
+
+
+
+
 
