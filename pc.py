912a6e9b3da24df5ab8c103a763d580ab679dca0
Nahuel Persia <npersia@localhost.localdomain>
2017-05-07 00:56:23 -0300

    agrego unas funcionalidad para traer productos

--- pc.py
+++ pc.py
@@ -23,7 +23,7 @@ def geti():
 
 #devuelve los comercios
 def getComercios(epicentro="",nivel=3):
-    offset = 3
+    offset = 30
     epic = "lat=-34.5639222&lng=-58.45990419999998"#epicentro
     headers = {"x-api-key": "zIgFou7Gta7g87VFGL9dZ4BEEs19gNYS1SOQZt96","Referer": "https://www.preciosclaros.gob.ar/","Origin": "https://www.preciosclaros.gob.ar/","User-Agent": None}
     suc = []
@@ -46,37 +46,71 @@ def getProductos(comercios):
     offset = 100
     headers = {"x-api-key": "zIgFou7Gta7g87VFGL9dZ4BEEs19gNYS1SOQZt96","Referer": "https://www.preciosclaros.gob.ar/","Origin": "https://www.preciosclaros.gob.ar/","User-Agent": None}
     prod = []
-    for x in range(99999):
+    
+    x = 0
+    PAGINAS = 1
+    while PAGINAS > x:
         url= "https://d3e6htiiul5ek9.cloudfront.net/prod/productos?string=leche&array_sucursales="+com+"&offset="+str(offset*x)+"&limit="+str(offset)
         r = requests.get(url,headers=headers)
+        #print(r.json()["productos"])
         prod.extend(r.json()["productos"])
-        if (offset*x) > int(r.json()["total"]):
-            break
+        TOTAL = int(r.json()["total"])
+        if TOTAL%offset > 0:
+            PAGINAS = (TOTAL//offset) + 1
+        else:
+            PAGINAS = (TOTAL//offset)
+        x += 1
+        print(TOTAL)
     return prod
     
 
 
-#supongo que llegan 21 elementos
+#devuelve los productos para una lista de Comercios, maximo para 50 comercios
+#def getProductos(comercios):
+#    com = str(comercios).replace("'","").replace("[","").replace("]","")
+#    offset = 100
+#    headers = {"x-api-key": "zIgFou7Gta7g87VFGL9dZ4BEEs19gNYS1SOQZt96","Referer": "https://www.preciosclaros.gob.ar/","Origin": "https://www.preciosclaros.gob.ar/","User-Agent": None}
+#    prod = []
+#    for x in range(99999):
+#        url= "https://d3e6htiiul5ek9.cloudfront.net/prod/productos?array_sucursales="+com+"&offset="+str(offset*x)+"&limit="+str(offset)
+#        r = requests.get(url,headers=headers)
+#        print(r.json()["productos"])
+#        prod.extend(r.json()["productos"])
+#        if (offset*x) > int(r.json()["total"]):
+#            break
+#    return prod
+
+
+
+
+
+
+
+
+#Cuando sean muchos comercios esta funcion sabe partirlos
 def getProd(comercios):
-    MAX_COMERCIOS = 5
+    MAX_COMERCIOS = 50
+    PRODUCTOS = []
     if len(comercios) <= MAX_COMERCIOS:
-        print("nada que hacer")
+        return getProductos(comercios)
     else:
         if len(comercios)%MAX_COMERCIOS > 0:
             HASTA = (len(comercios)/MAX_COMERCIOS)+1
         else:
             HASTA = len(comercios)/MAX_COMERCIOS
         for x in range(0,int(HASTA)):
-            print(comercios[x*MAX_COMERCIOS:(x+1)*MAX_COMERCIOS])
+            PRODUCTOS.extend(getProductos(comercios[x*MAX_COMERCIOS:(x+1)*MAX_COMERCIOS]))
+        return PRODUCTOS
 
 
 
-#c = getComercios(nivel=3)
-#print(c)
-#idC = getIdComercios(c)
-#print(idC)
+c = getComercios(nivel=3)
+print(c)
+idC = getIdComercios(c)
+print(idC)
 #print(getProductos(idC))
-
-
-getProd([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
+#print(len(idC))
+#prods=getProd(idC)
+#print(prods)
+#print(len(prods))
 
